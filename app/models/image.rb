86f98913dfe08242c25491568d1124dcafe8634c
Lucas Shaffer <lucas.shaffer@appfolio.com>
2019-10-22 13:24:01 -0700

    ls - add tag validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ApplicationRecord
   validates :url, presence: true
-  validates_format_of :url, :multiline => true, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates_format_of :url, multiline: true, with: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates_format_of :tag_list, multiline: true, with: /^$|^([a-z]+)(,\s*[a-z]+)*$/i, message: 'must be a comma separated list'
   acts_as_taggable
 end
